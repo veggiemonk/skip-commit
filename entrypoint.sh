449e94fa83e7918c4079f37322205e17b868f993
Julien Bisconti <julien.bisconti@gmail.com>
2019-01-25 21:00:07 +0100

    Fix implicit script exit on assigning 0

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,14 @@
 set -e
 
 # Get last commit message
-last_commit_log=$(git log -1 --pretty=format:"%s")
+readonly local last_commit_log=$(git log -1 --pretty=format:"%s")
 echo "last commit log: $last_commit_log"
 
-filter_count=$(echo "$last_commit_log" | grep -c "$COMMIT_FILTER")
-echo "filter_count $filter_count"
+readonly local filter_count=$(echo "$last_commit_log" | grep -c "$COMMIT_FILTER" )
+echo "number of occurence of '$COMMIT_FILTER' in '$last_commit_log': $filter_count"
 
 if [[ "$filter_count" -eq 0 ]]; then
-  echo "all good"
+  echo "all good, continue"
 else
   echo "the last commit log \"$last_commit_log\" contains \"$COMMIT_FILTER\", stopping"
   exit 78
